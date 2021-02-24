92780fc8630a00b6bb0a79afbf78581a7c511cfd
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-02-24 17:49:58 +0000

    added search functionality to nonFood page

--- client/src/components/nonFoodItems/NonFoodPage.js
+++ client/src/components/nonFoodItems/NonFoodPage.js
@@ -1,12 +1,31 @@
+import {useEffect, useState} from 'react';
+import FilterForm from "../auxillary/FilterForm";
 import NonFoodList from "../nonFoodItems/NonFoodList";
 
 const NonFoodPage = ({nonFoodData, setIndivdualNonFoodItem, user}) => {
+
+    const [filteredEquip, setFilteredEquip] = useState([]);
+
+    useEffect(()=> {
+        setFilteredEquip(nonFoodData)
+    }, [nonFoodData])
+
+    const filterData = (searchTerm) => {
+        if(searchTerm) {
+            const someEquip = nonFoodData.filter((item) => {
+                return item.type.toUpperCase().includes(searchTerm.toUpperCase())
+            });
+            setFilteredEquip(someEquip)
+        } else {
+            setFilteredEquip(nonFoodData)
+        }
+    }
+
      return (
-         <>
-            <NonFoodList nonFoodData={nonFoodData} setIndivdualNonFoodItem={setIndivdualNonFoodItem}
-            user={user}
-            />
-         </>
+         <div>
+             <FilterForm filterData={filterData}/>
+            <NonFoodList nonFoodData={filteredEquip} setIndivdualNonFoodItem={setIndivdualNonFoodItem} user={user} />
+         </div>
      )
 }
 
